d29046a5e208ceca659524e4a41bb2f17d3b665c
Gregory Hitz <hitzg@ethz.ch>
2015-02-22 22:03:54 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pdfpeeler',
+      version='1.0',
+      description='Remove margins from pdfs',
+      author='Gregory Hitz',
+      scripts=['pdfpeeler.py'],
+      )
